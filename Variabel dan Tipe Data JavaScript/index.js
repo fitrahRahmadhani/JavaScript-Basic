6bf3a781b3c9b9304c1b7d374351325f54ca7c0d
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2023-03-21 06:28:35 +0700

    Variabel dan Tipe Data

---
+++ Variabel dan Tipe Data JavaScript/index.js
@@ -0,0 +1,35 @@
+// Tipe Data
+let namaVariabel1 = "hello";
+const namavariabel2 = "world";
+
+// Contoh penulisan nama variabel yang benar
+let nama;
+let tempatTinggal;
+let _nomorHandphone;
+let $nomorAkunBank;
+
+// Contoh penulisan nama variabel yang salah
+/*
+let makanan pembuka; // tidak bolah ada spasi
+let @username; // tidak boleh diawali dengan @
+let delete; // delete itu merupakan kata kunci di JavaScript
+*/
+
+console.log(namaVariabel1 + " " + namavariabel2);
+
+let namaLengkap = "Stefanus";
+const namaIbuKandung = "Clara"; //String
+const umur = 20; // const tidak bisa diubah valuenya, kecuali array dtau objek
+const apakahSudahMenikah = false; // boolean
+let jumlahMotor = 1; // integer
+let phi = 3.14; // float
+let berapaKaliNaikHaji; // undefined -> belum diberi sebuah nilai
+
+console.log(namaLengkap);
+console.log(namaIbuKandung);
+console.log(umur);
+console.log(apakahSudahMenikah);
+console.log(jumlahMotor);
+console.log(berapaKaliNaikHaji);
+
+
