299be0620d12a904109d348fd3abc710fed35d3f
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2023-04-02 18:49:03 +0700

    Update fungsi

--- Fungsi/script.js
+++ Fungsi/script.js
@@ -46,3 +46,19 @@ function operasiPerkalian(angka1, angka2){
     return angka1 * angka2;
 }
 console.log(operasiPerkalian(2,6));
+
+// LINGKUP GLOBAL VS LINGKUP LOKAL
+const olahraga = 'basketball';
+function namaAtlet(){
+    let atlet = 'Leonel Messi';
+    const noMessi = 10;
+    console.log(olahraga); //output basketball
+    if(olahraga === 'basketball'){
+        atlet = 'Kobe Bryant';
+        const noKobe = 24;
+        console.log(olahraga);
+    }
+    return atlet;
+}
+console.log(namaAtlet()); // Output: Kobe Bryant
+console.log(olahraga); // Output:basketball 
